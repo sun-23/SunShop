260405bd21319c4aa63ba6fecab462956f2492df
sun <twinsamson@gmail.com>
2019-04-16 15:34:46 +0700

    back ok

--- SunShop/SunShop/RegisterViewController.swift
+++ SunShop/SunShop/RegisterViewController.swift
@@ -14,17 +14,22 @@ class RegisterViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }//Main Method
+    
+    
+    
+    
+    
+    @IBAction func backButton(_ sender: UIBarButtonItem) {
+        
+        performSegue(withIdentifier: "BackToAuthen", sender: nil)
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func uploadButton(_ sender: UIBarButtonItem) {
     }
-    */
+    
+    
 
-}
+}// Main Class
