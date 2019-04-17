9cf00556b9fd3b5a4f714b20c207f9666e0267df
sun <twinsamson@gmail.com>
2019-04-17 10:36:57 +0700

    Check Space Success

--- SunShop/SunShop/RegisterViewController.swift
+++ SunShop/SunShop/RegisterViewController.swift
@@ -9,6 +9,21 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
+    
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    
+    @IBOutlet weak var userTextField: UITextField!
+    
+    
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +43,30 @@ class RegisterViewController: UIViewController {
     
 
     @IBAction func uploadButton(_ sender: UIBarButtonItem) {
+        
+        print("You Click Upload")
+        
+        //Get Value From TextField
+        let name = nameTextField.text!
+        let user = userTextField.text!
+        let password = passwordTextField.text!
+        
+        //Show Message on Console
+        print("name ==>> \(name)")
+        print("user ==>> \(user)")
+        print("password ==>> \(password)")
+        
+        //Check Space
+        
+        if ((name.count == 0) || (user.count == 0) || (password.count == 0)) {
+            print("Have Space")
+        } else {
+            print("No Space")
+        }
+        
+
+        
+        
     }
     
     
